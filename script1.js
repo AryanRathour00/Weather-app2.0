98617d101a1b382c0c9c71cd4ebcc21220abc409
= <=>
2024-06-21 10:18:06 +0530

    changing the files for ajax programmming

---
+++ script1.js
@@ -0,0 +1,51 @@
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.querySelector("#searchBtn");
+const weather_img = document.querySelector(".weather-img");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const humidity = document.querySelector("#humidity");
+const wind_speed = document.querySelector("#wind-speed");
+const location_not_found = document.querySelector(".location_not_found");
+const weather_body = document.querySelector(".weather-body");
+
+async function checkWeather(city) {
+  const api_key = "80345281fd72dd0e212cdcd1553fbae8";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  const weather_data = await fetch(`${url}`).then((response) =>
+    response.json()
+  );
+  if (weather_data.cod === "404") {
+    location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    return;
+  }
+  location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
+  temperature.innerHTML = `${Math.floor(weather_data.main.temp - 273.15)}°C`;
+  humidity.innerHTML = `${Math.floor(weather_data.main.humidity)}%`;
+  description.innerHTML = `${weather_data.weather[0].description}`;
+  wind_speed.innerHTML = `${weather_data.wind.speed}Km/hr`;
+
+  switch (weather_data.weather[0].main) {
+    case "Clouds":
+      weather_img.src = "/assets/cloud.png";
+      break;
+    case "Clear":
+      weather_img.src = "/assets/clear.png";
+      break;
+    case "Rain":
+      weather_img.src = "/assets/rain.png";
+      break;
+    case "Mist":
+      weather_img.src = "/assets/mist.png";
+      break;
+    case "Snow":
+      weather_img.src = "/assets/snow.png";
+      break;
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
